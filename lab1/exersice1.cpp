c980d7fe5d6f762308d4a9c5571ca1218f1ba6e1
OlexanderVanzuriak <vanzuriak.oleksandr@chnu.edu.ua>
2023-02-05 10:17:01 +0200

    make better readable

--- lab1/exersice1.cpp
+++ lab1/exersice1.cpp
@@ -7,15 +7,15 @@ int main(int argc, char const *argv[])
     double A[MAX], B[MAX];
     int nA, nB, i;
     do {
-        cout<<"Enter the massiv A number of element < "<<MAX<<endl;
+        cout<<"Enter the array A number of element < "<<MAX<<": ";
         cin>>nA;
     } while(nA<0 || nA>MAX);
     for(i=0; i<nA; i++) {
         cout<<"A["<<i<<"]=";
         cin>>A[i];
-        if(A[i] > 0) B[nB++] = A[i];
+        if(A[i] >= 0) B[nB++] = A[i];
     }
-    cout<<endl;
+    cout<<"B array:"<<endl;
     for(i=0; i<nB; i++) {
         cout<<"B["<<i<<"]="<<B[i]<<endl;
     }
